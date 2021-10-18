8d40e1af75e6587e57913fe060603647cef369ab
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-10-18 17:58:21 +0200

    :zap:  add project solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Button from "./Button";
+import Input from "./Input";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const increment = () => {
+    if (number < 10) {
+      setNumber(number + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (number > 10) {
+      setNumber(number - 1);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button text="-" actionOnClick={decrement} />
+      <Input number={number} />
+      <Button text="+" actionOnClick={increment} />
+    </>
   );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,9 @@
+function Input({ number }) {
+  return (
+    <>
+      <input type="text" readOnly value={number}></input>
+    </>
+  );
+}
+
+export default Input;
